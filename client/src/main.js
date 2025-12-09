6813c9baaecb4c43eeecdacd94eacbef5672a230
Ghignatti Nicolò <94747281+NickGhignatti@users.noreply.github.com>
2025-12-09 15:45:48 +0100

    Fix remove useless files and failing CI missing (#31)
    * fix remove useless js files
    
    * fix missing CI

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import '@phosphor-icons/web/regular'
-import '@phosphor-icons/web/bold'
-import '@phosphor-icons/web/fill'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
