25572bf40f3478bafbcafa895e6cd99a954d2c84
NickGhignatti <nick.ghignatti@gmail.com>
2025-12-09 14:57:57 +0100

    Add CI workflow to run server tests

--- server/__tests__/setup.ts
+++ server/__tests__/setup.ts
@@ -1,6 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { beforeAll, afterAll, afterEach } from 'vitest';
 
 let mongoServer: MongoMemoryServer;
 
