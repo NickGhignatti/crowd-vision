7aca1d919e580a01c4ea819757fccebbddb02c4e
NickGhignatti <nick.ghignatti@gmail.com>
2025-12-14 17:14:06 +0100

    Remove useless transactions

--- client/src/components/auth/LoginModal.vue
+++ client/src/components/auth/LoginModal.vue
@@ -1,10 +1,8 @@
 <template>
   <Teleport to="body">
     <Transition
-      enter-active-class="transition duration-200 ease-out"
       enter-from-class="opacity-0 scale-95"
       enter-to-class="opacity-100 scale-100"
-      leave-active-class="transition duration-150 ease-in"
       leave-from-class="opacity-100 scale-100"
       leave-to-class="opacity-0 scale-95"
     >
