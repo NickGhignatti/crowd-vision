54cd9e40f78d6f3f4b3654af675face1fa41d46e
NickGhignatti <nick.ghignatti@gmail.com>
2026-01-26 09:49:22 +0100

    feat: openapi documentation endpoint

--- server/auth-service/src/index.ts
+++ server/auth-service/src/index.ts
@@ -1,14 +1,19 @@
-import express from 'express'
+import express from 'express';
 import cors from "cors";
+import swaggerUi from 'swagger-ui-express';
+import YAML from "yamljs";
 import router from "./router.js";
 import {connectMongo} from "./config/db.js";
 
 export const app = express()
 const PORT = 3000;
 
+const swaggerDocument = YAML.load('./openapi.yaml');
+
 app.use(cors());
 app.use(express.json());
-app.use('/', router)
+app.use('/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 if (process.env.NODE_ENV !== 'test') {
     connectMongo().then(() => {
