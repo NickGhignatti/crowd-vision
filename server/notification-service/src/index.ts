a7bff4ebe8cf26134f0930eca867da6ab71d4986
NickGhignatti <nick.ghignatti@gmail.com>
2026-02-06 10:01:16 +0100

    update openapis

--- server/notification-service/src/index.ts
+++ server/notification-service/src/index.ts
@@ -2,19 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import router from './router.js';
+import swaggerUi from 'swagger-ui-express';
 import { connectRedis } from './config/redis.js';
 import { startNotificationLoop } from './services/notificationService.js';
 import {connectMongo} from "./config/db.js";
+import YAML from "yamljs";
 
 dotenv.config();
 
 export const app = express();
 const PORT = process.env.PORT || 3000;
 
+const swaggerDocument = YAML.load('./openapi.yaml');
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const startServer = async () => {
     await connectRedis();
