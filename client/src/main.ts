b63129421ea3274da5d5a48d4f225b0dde6ef6ef
NickGhignatti <nick.ghignatti@gmail.com>
2026-04-22 10:41:23 +0200

    fix: import tres at compile time

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,12 +5,14 @@ import '@phosphor-icons/web/fill'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Tres from '@tresjs/core'
 import App from './App.vue'
 import router from './router'
 import i18n from '@/i18n.ts'
 
 const app = createApp(App)
 
+app.use(Tres)
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
