f35af05f038a9be6470afafd4009a07d5f03c4c5
Mounir Samite <mounir.samite@gmail.com>
2026-05-02 11:23:24 +0200

    chore: rebase the current branch

--- server/agent-service/src/schemas/llm.schema.ts
+++
@@ -1,7 +0,0 @@
-import { Type, Static } from "@sinclair/typebox";
-
-export const MessageLLMSchema = Type.Object({
-  msg: Type.String(),
-});
-
-export type messageLLM = Static<typeof MessageLLMSchema>;
