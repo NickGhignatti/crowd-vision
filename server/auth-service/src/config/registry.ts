f85de7545fe41a00bb2f83da96d5939fed4c692a
NickGhignatti <nick.ghignatti@gmail.com>
2026-05-05 12:08:43 +0200

    add no. errors as metrics

--- server/auth-service/src/config/registry.ts
+++ server/auth-service/src/config/registry.ts
@@ -17,6 +17,13 @@ export const httpRequestsTotal = new Counter({
   registers: [register],
 });
 
+export const httpRequestsError = new Counter({
+  name: "http_error_requests_total",
+  help: "Total number of errors on the HTTP requests",
+  labelNames: ["method", "route", "status_code"],
+  registers: [register],
+});
+
 export const httpRequestDuration = new Histogram({
   name: "http_request_duration_seconds",
   help: "Duration of HTTP requests in seconds",
--- server/auth-service/src/middlewares/metrics.ts
+++ server/auth-service/src/middlewares/metrics.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import { httpRequestsTotal, httpRequestDuration } from "../config/registry.js"
+import { httpRequestsTotal, httpRequestDuration, httpRequestsError } from "../config/registry.js"
 
 export const metricsMiddleware = (
   req: Request,
@@ -23,6 +23,7 @@ export const metricsMiddleware = (
 
     httpRequestsTotal.inc(labels);
     httpRequestDuration.observe(labels, duration);
+    if (req.statusCode || 0 >= 400) httpRequestsError.inc(labels)
   });
 
   next();
